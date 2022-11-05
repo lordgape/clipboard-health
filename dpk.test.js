dda4c2882a6633fbfaae13bee598b5b6e45a05a1
Chesvic Lordgape <lordgape@gmail.com>
2022-11-05 14:19:19 +0100

    initial commit

---
+++ dpk.test.js
@@ -0,0 +1,13 @@
+const { deterministicPartitionKey } = require("./dpk");
+
+describe("deterministicPartitionKey", () => {
+  it("Returns the literal '0' when given no input", () => {
+    const trivialKey = deterministicPartitionKey();
+    expect(trivialKey).toBe("0");
+  });
+  
+  it("Returns the literal 0 when given no input", () => {
+    const trivialKey = deterministicPartitionKey();
+    expect(trivialKey).toBe("0");
+  });
+});
